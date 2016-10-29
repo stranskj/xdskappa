47da0ad142bf15da6a8c4738c728f88adc0f5151
Jan Stransky <stransky@ibt.cas.cz>
2016-10-29 22:26:30 +0200

    Final version of run_xds.py. Working.

--- bin/run_xds.py
+++ bin/run_xds.py
@@ -3,6 +3,7 @@
 import common, os
 from xdsinp import XDSINP
 
+
 def ParseInput():
         parser = argparse.ArgumentParser(prog= 'xdskappa.run_xds', description='Modifies existing XDS.INPs, run XDS and shows overall statistics (no scaling)', epilog='Dependencies: XDS, gnuplot')
 
@@ -19,6 +20,8 @@ def ParseInput():
         parser.add_argument('-P','--parameter-file', dest='XDSParameterFile', nargs='?', default=None, const='XDSKAPPA.INP',metavar='FILE', help='File with list of parameters to modify XDS.INP files. Parameters format as defined for XDS.INP. When no file given, "XDSKAPPA.INP" is expected.')
 
         parser.add_argument('-r','--reference-dataset', dest='ReferenceData', metavar='DATASET', help='Name of reference dataset from working list. The first one used by default. For external reference dataset use: -p REFERENCE_DATA_SET= path/data/XDS_ASCII.HKL')
+        
+        parser.add_argument('--no-run', dest='norun', action='store_false',default=True, help="Only modify XDS.INP, don't run xds_par.")
 
         parser.add_argument('-f', '--force', dest='ForceXDS', action='store_true', help='Force integration on unsuccesfull indexing.')
 #      parser.add_argument('-opt','--optimize', dest='OptIntegration', nargs='?', action='append', const= 'ALL', metavar='ALL FIX BEAM GEOMETRY', help='Run XDS twice, with optimized parameters in second run. FIX - fix parameters in integration; BEAM - copy BEAM parameters from INTEGRATE.LP; GEOMETRY - copy GXPARM.XDS to XPARM.XDS. One keyword per parameter occurance. When given without a value, ALL is presumed.')
@@ -40,7 +43,7 @@ def main():
     print " "
 
     in_data = ParseInput()
-    #print in_data
+    print in_data
 
     if os.path.isfile(in_data.DatasetListFile):
         datasets,names = common.ReadDatasetListFile(in_data.DatasetListFile)
@@ -64,6 +67,14 @@ def main():
                 print "Skipping."
                 continue
 
+    if in_data.norun:
+        common.RunXDS(names)
+        
+        if in_data.ForceXDS:
+            common.ForceXDS(names)
+        
+        common.PrintISa(names)
+    return                
 
 if __name__ == "__main__":
     import sys
