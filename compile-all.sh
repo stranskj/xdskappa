035a4dc3641b2ea320649f82c3228cfd57c63aba
Jan Stránský <jan.stransky@ibt.cas.cz>
2019-02-15 12:05:30 +0100

    Build script update to Python 2.7.15, local paths

--- compile-all.sh
+++ compile-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #run as root, or docker priviledged
 
-./compile-fedora.sh
+#./compile-fedora.sh
 ./compile-centos7.sh
 #docker run -t -v /home/stransky/skripty/xdskappa:/src/xdskappa --user=$UID debian:pyinst2 /bin/bash -c "cd /src/xdskappa; ./compile-debian.sh"
-chown -R stransky:users /home/stransky/skripty/xdskappa/release/
+chown -R stransky:users `pwd`/xdskappa/release/
 
